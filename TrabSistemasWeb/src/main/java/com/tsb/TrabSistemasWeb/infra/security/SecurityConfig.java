874673135b251076f9686f1a2563a4a892751fc9
Igor Westermann <i.westma@gmail.com>
2024-09-13 20:39:02 -0300

    add swagger

--- TrabSistemasWeb/src/main/java/com/tsb/TrabSistemasWeb/infra/security/SecurityConfig.java
+++ TrabSistemasWeb/src/main/java/com/tsb/TrabSistemasWeb/infra/security/SecurityConfig.java
@@ -18,15 +18,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private static final String[] SWAGGER_WHITELIST = {
-            "/swagger-vi/**",
-            "/v3/api-docs/**",
-            "/ swagger-resources/**",
-            "/ swagger-resources"
-    };
-
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
 
     @Autowired
     SecurityFilter securityFilter;
@@ -38,8 +29,14 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers("/**").hasAnyRole("ADMIN") // Secure admin endpoints
+                        .requestMatchers(HttpMethod.POST, "/auth/register").hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/**").hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/**").hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/**").hasAnyRole("ADMIN")
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/swagger-resources/**").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
